96663f274460a783e9c5aaf4a0f9c9d037dbd7c5
lopongoaca <contacto@lopongoaca.com>
2023-06-15 15:36:06 -0500

    feat: :page_facing_up: Add politica

---
+++ app/privacy/page.js
@@ -0,0 +1,9 @@
+export default function Privacy() {
+    return (
+        <div className="container mx-auto px-10 py-5">
+            <h3 className="font-bold text-2xl ">Política de Privacidad</h3>
+            <p>
+                Esta extensión no recoge ningún tipo de  información del cliente o del navegador, toda la información es mostrada de manera local y en tiempo real al usuario.</p>
+        </div>
+    )
+}
