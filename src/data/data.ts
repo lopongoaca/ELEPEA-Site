cee066427ef067ea2bd3aafee72190bc29df70d1
Andres Atehortua <contacto@lopongoaca.com>
2024-03-03 18:29:41 -0500

    Add and delete client logos

---
+++ src/data/data.ts
@@ -0,0 +1,84 @@
+export const webServicies = [
+	"Web Development",
+	"Mobile Development",
+	"Web Design",
+	"SEO",
+	"E-commerce",
+	"Hosting",
+	"Analytics",
+	"Web Optimization",
+];
+
+export const brandingServicies = [
+	"Logo Design",
+	"Brand Identity",
+	"Brand Strategy",
+	"Brand Guidelines",
+	"Brand Consultancy",
+	"Brand Development",
+	"Brand Management",
+	"Brand Marketing",
+];
+
+export const clients = [
+    {
+        name: "Bagels",
+        logo: "bagels.png",
+    },
+    {
+        name:"Lv Lending",
+        logo:"lv-lending.png",
+    },
+    {
+        name: "Creactivo",
+        logo: "creactivo.png",
+    },
+    {
+        name: "Dalias",
+        logo: "dalias.png",
+    },
+    {
+        name:"Cercis",
+        logo: "cercis.png",
+    },
+    {
+        name: "Ekho",
+        logo: "ekho.png",
+    },
+    {
+        name: "Enerlat",
+        logo: "enerlat.png",
+    },
+    {
+        name: "Ethics",
+        logo: "ethics.png",
+    },
+    {
+        name:"VML Abogados",
+        logo: "vml.png",
+    },
+    {
+        name: "Fruty Green",
+        logo: "fruty-green.png",
+    },
+    {
+        name: "Green Plus",
+        logo: "green-plus.png",
+    },
+    {
+        name: "Monthgomery Fair",
+        logo: "mcagfair.png",
+    },
+    {
+        name: "Linkvest",
+        logo: "linkvest.png",
+    },
+    {
+        name: "Panacambios",
+        logo: "panacambios.png",
+    },
+    {
+        name: "Solutec",
+        logo: "solutec.png",
+    },
+];
